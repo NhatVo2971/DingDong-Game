6bfc27cfbceedac47eb41866e242ea5f574f297c
NhatVo2971 <s3868763@rmit.edu.vn>
2023-08-25 23:21:48 +0700

    Initial Commit

---
+++ DingDong Game/DingDong_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DingDong_GameApp.swift
+//  DingDong Game
+//
+//  Created by Võ Phúc Duy Nhật on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DingDong_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
